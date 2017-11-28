2bbab76c7eadd195e6edd3d2a52ba7ffbb4466a3
Ted-Koomen <koomen.ted@gmail.com>
2017-11-28 11:58:52 -0500

    initial commit and set up register

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+     def new 
+        user = User.new(username: params[:userName], email: params[:email], password_digest: params[:password], access_token:SecureRandom.base64)
+        if user.save
+            render :json => {:status => :ok}
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:userName, :password, :email)
+    end
+end
