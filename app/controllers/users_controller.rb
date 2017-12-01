f1702dea4374f06aeacfac960d838e10fed42805
Ted-Koomen <koomen.ted@gmail.com>
2017-12-01 09:42:48 -0500

    and rendering error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
      def new 
-        user = User.new(username: params[:userName], email: params[:email], password_digest: params[:password], access_token:SecureRandom.base64)
-        if user.save
-            render :json => {:status => :ok}
+        user = User.new(username: params[:userName], email: params[:email], password_digest: params[:password], access_token: SecureRandom.base64)
+        if !user.save
+            render json: user.errors.full_messages
+            p user.errors.full_messages
         end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-
+    validates_presence_of :username, :email, :password_digest, :access_token
+    validates_uniqueness_of :username, :email, :access_token
 end
